c8457f14ec696b5fb7c7fc85bb45fd03c6c30a3d
Chinmoy Bhuyan <b17cs082@cit.ac.in>
2025-07-25 15:04:24 +0530

    Fix PlatformIO build

--- examples/esp32_demo/main.cpp
+++ examples/esp32_demo/main.cpp
@@ -1,4 +1,12 @@
 #include <stdio.h>
-extern "C" void app_main(){
+
+#ifdef ESP_PLATFORM
+extern "C" void app_main() {
     printf("ISO15118 port example\n");
 }
+#else
+int main() {
+    printf("ISO15118 port example\n");
+    return 0;
+}
+#endif
